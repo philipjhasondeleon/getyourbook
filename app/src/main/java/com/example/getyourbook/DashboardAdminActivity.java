c1f7869dc9f2f7113dca07fc648ae8d83ef0c215
philipjhasondeleon <jhasondeleon@gmail.com>
2021-11-21 12:28:14 -0500

    authenticate user using firebase

---
+++ app/src/main/java/com/example/getyourbook/DashboardAdminActivity.java
@@ -0,0 +1,47 @@
+package com.example.getyourbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.getyourbook.databinding.ActivityDashboardAdminBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardAdminActivity extends AppCompatActivity {
+
+    private ActivityDashboardAdminBinding binding;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
+    }
+
+    private void checkUser() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser==null){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+        else {
+            String email = firebaseUser.getEmail();
+            binding.subTitleTv.setText(email);
+        }
+    }
+}
