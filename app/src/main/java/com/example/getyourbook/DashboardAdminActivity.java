948e6bcc815f4a0827661bc37a9858b913089bfe
philipjhasondeleon <jhasondeleon@gmail.com>
2021-11-22 14:55:11 -0500

    Show book category

--- app/src/main/java/com/example/getyourbook/DashboardAdminActivity.java
+++ app/src/main/java/com/example/getyourbook/DashboardAdminActivity.java
@@ -1,19 +1,33 @@
 package com.example.getyourbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import com.example.getyourbook.databinding.ActivityDashboardAdminBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
     private ActivityDashboardAdminBinding binding;
     private FirebaseAuth firebaseAuth;
+
+    private ArrayList<ModelCategory> categoryArrayList;
+
+    private AdapterCategory adapterCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,8 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+        loadCategories();
+
 
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +52,30 @@ public class DashboardAdminActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(DashboardAdminActivity.this, CategoryAddActivity.class));
+            }
+        });
+    }
+
+    private void loadCategories() {
+        categoryArrayList = new ArrayList<>();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                categoryArrayList.clear();
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+
+                    categoryArrayList.add(model);
+                }
+                adapterCategory = new AdapterCategory(DashboardAdminActivity.this, categoryArrayList);
+                binding.categoriesRv.setAdapter(adapterCategory);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
